f2160350261c4cd96912121be6a66a3a0e106c5f
Abhijit <akarmakar846@gmail.com>
2023-06-29 08:59:52 +0530

    Some changes in server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ mongoose.connect(DB)
 
 /* DEFAULT ROUTE */
 app.get('/', (req, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(200).json({ message: "Welcome to the Authentication API." });
 });
 
